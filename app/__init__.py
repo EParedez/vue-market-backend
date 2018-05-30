698ca54f228199cb9ea549d9ca02716bbdd359f1
BritoParedes17 <gabrielbrito600@gmail.com>
2018-05-30 18:26:13 -0500

    Tutorial No. 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import os
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
 from flask import Flask
 #from flask.ext.sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
@@ -6,6 +10,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config.from_object('config')
 db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
 
